d44a1d4b0a08ee16bac9bfb683621df29e54418d
brendapsd <brenda.sd.15@gmail.com>
2019-09-10 06:37:18 -0500

    Updates

---
+++ precache-manifest.75f6869b85c8b7118d818d84b3b823f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1092c3d842a36b22b59e21b3f905308d",
+    "url": "/Reto-Rimac/index.html"
+  },
+  {
+    "revision": "640338afadd9313be798",
+    "url": "/Reto-Rimac/static/css/main.bbf9445f.chunk.css"
+  },
+  {
+    "revision": "4266660d8840f8848b1e",
+    "url": "/Reto-Rimac/static/js/2.cdc2438f.chunk.js"
+  },
+  {
+    "revision": "640338afadd9313be798",
+    "url": "/Reto-Rimac/static/js/main.63674b7e.chunk.js"
+  },
+  {
+    "revision": "741dd2816257079c8b6e",
+    "url": "/Reto-Rimac/static/js/runtime~main.df309e52.js"
+  }
+]);
